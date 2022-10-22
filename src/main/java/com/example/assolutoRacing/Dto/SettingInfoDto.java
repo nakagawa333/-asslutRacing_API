91d3b0363be0b206fd5654b9654214457ce9f31e
nakagawa331 <Nakagawasousei333>
2022-10-22 18:31:27 +0900

    名称ミス asbをabsに変更

--- src/main/java/com/example/assolutoRacing/Dto/SettingInfoDto.java
+++ src/main/java/com/example/assolutoRacing/Dto/SettingInfoDto.java
@@ -2,11 +2,9 @@ package com.example.assolutoRacing.Dto;
 
 import java.math.BigDecimal;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class SettingInfoDto {
 	//id
 	public Integer id;

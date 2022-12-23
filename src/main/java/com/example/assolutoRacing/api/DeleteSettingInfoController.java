b49b381a54546019b9536ca2dd97780fbc1c6835
nakagawa331 <Nakagawasousei333>
2022-12-24 00:39:09 +0900

    コメント追加

--- src/main/java/com/example/assolutoRacing/api/DeleteSettingInfoController.java
+++ src/main/java/com/example/assolutoRacing/api/DeleteSettingInfoController.java
@@ -42,7 +42,8 @@ public class DeleteSettingInfoController {
 			throw e;
 		}
 		
-		Boolean deleteSucessFlag = deleteCount == 0 ? true : false;
+		//削除件数が1件の場合成功
+		Boolean deleteSucessFlag = deleteCount == 1 ? true : false;
 
 		HttpHeaders headers = new HttpHeaders();
 		ResponseEntity<Boolean> resEntity = new ResponseEntity<>(deleteSucessFlag,headers,HttpStatus.CREATED);

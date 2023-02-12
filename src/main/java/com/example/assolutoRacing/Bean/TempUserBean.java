d9b1c654491ecf6ee1368bab598c7706601df1db
nakagawa333 <naka33321@gmail.com>
2023-02-12 12:05:47 +0900

    仮ユーザーBeanクラス アノテーション追加

--- src/main/java/com/example/assolutoRacing/Bean/TempUserBean.java
+++ src/main/java/com/example/assolutoRacing/Bean/TempUserBean.java
@@ -1,5 +1,7 @@
 package com.example.assolutoRacing.Bean;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -7,17 +9,30 @@ import com.example.assolutoRacing.Constants.Constants;
 
 import lombok.Data;
 
+/**
+ * 仮ユーザーBeanクラス
+ * @author nakagawa.so
+ *
+ */
 @Data
 public class TempUserBean {
 	//ユーザー名
 	@NotBlank
+	@Max(100)
 	private String userName;
+	
 	//メール
+	@NotBlank
 	@Pattern(regexp = Constants.REGEX.MAIL)
+	@Max(100)
 	private String mail;
+	
 	//パスワード
 	@NotBlank
+	@Max(100)
+	@Min(7)
 	private String password;
+	
 	//リクエストurl
 	@NotBlank
 	private String requestUrl;

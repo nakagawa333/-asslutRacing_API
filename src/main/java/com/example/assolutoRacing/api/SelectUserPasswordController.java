b170280c90e3103a8b44811303767e3cf34bb343
nakagawa333 <naka33321@gmail.com>
2023-03-05 17:21:09 +0900

    不要ライブラリ削除

--- src/main/java/com/example/assolutoRacing/api/SelectUserPasswordController.java
+++ src/main/java/com/example/assolutoRacing/api/SelectUserPasswordController.java
@@ -7,16 +7,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.assolutoRacing.Bean.SelectUserBean;
 import com.example.assolutoRacing.Bean.SelectUserPasswordBean;
 import com.example.assolutoRacing.Bean.UserRes;
-import com.example.assolutoRacing.Constants.Constants;
 import com.example.assolutoRacing.Service.UserService;
 
 /**

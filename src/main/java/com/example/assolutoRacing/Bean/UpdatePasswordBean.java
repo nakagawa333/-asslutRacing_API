a0de9069025859f0f82772bf8b76715356f2cb84
nakagawa333 <naka33321@gmail.com>
2023-03-07 01:57:32 +0900

    バリデーション追加

--- src/main/java/com/example/assolutoRacing/Bean/UpdatePasswordBean.java
+++ src/main/java/com/example/assolutoRacing/Bean/UpdatePasswordBean.java
@@ -1,16 +1,20 @@
 package com.example.assolutoRacing.Bean;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 /**
- * パスワード更新
+ * パスワード更新Beanクラス
  * @author nakagawa.so
  *
  */
 @Data
 public class UpdatePasswordBean {
+	@NotBlank
 	//パスワード
 	private String password;
+	@NotBlank
 	//トークン
 	private String token;
 }

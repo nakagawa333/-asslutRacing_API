cbf2096525104613c409041b1ce612c8f0c8c1ea
nakagawa333 <naka33321@gmail.com>
2023-03-27 23:10:09 +0900

    Angular側からのリクエスト情報のAuthorizationを取得できない不具合の改修

--- src/main/java/com/example/assolutoRacing/config/WebSecurityConfig.java
+++ src/main/java/com/example/assolutoRacing/config/WebSecurityConfig.java
@@ -1,26 +1,15 @@
 package com.example.assolutoRacing.config;
 
-import java.io.UnsupportedEncodingException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.NullRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-
-import com.amazonaws.HttpMethod;
 import com.example.assolutoRacing.Service.CustomUserDetailsService;
 import com.example.assolutoRacing.Service.JwtUtil;
-import com.example.assolutoRacing.api.LoginController;
 
 @Configuration
 @EnableWebSecurity
@@ -35,11 +24,13 @@ public class WebSecurityConfig {
 	
 	@Bean
 	SecurityFilterChain springSecurity(HttpSecurity http) throws Exception {
-		http.cors().disable()
-		.csrf().disable()
+		http.csrf().disable()
 		.authorizeHttpRequests()
-		.antMatchers("/select/notification","/login","/settings/account").permitAll()
+		.antMatchers("/select/notification","/login","/signup","/password/reset").permitAll()
+		.antMatchers("/select/**").permitAll()
+		.antMatchers("/verify/**").permitAll()
 		.anyRequest().authenticated()
+		.and().cors()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()

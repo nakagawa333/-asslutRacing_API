1b7056305ee17906ad53fcc5a9423d4d50506f08
nakagawa333 <naka33321@gmail.com>
2023-04-02 15:20:52 +0900

    有効期限変更

--- src/main/java/com/example/assolutoRacing/Constants/Constants.java
+++ src/main/java/com/example/assolutoRacing/Constants/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	}
 	
 	public static class TOKEN{
-		public static final Date ACESSEXP = new Date(System.currentTimeMillis() + 30 * 60 * 60 * 24);
+		public static final Date ACESSEXP = new Date(System.currentTimeMillis() + 300 * 60 * 60 * 24);
 		public static final Date REFRESHEXP = new Date(System.currentTimeMillis() + 5000 * 60 * 60 * 24);
 	}
 }
